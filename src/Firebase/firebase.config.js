1e1a027bdec5bd5a57c13f38a6fb866bd4c2548d
lutfor779 <mdlrrahman779@gmail.com>
2021-12-08 13:07:52 +0600

    forgot to git

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,20 @@
+// console.log(process.env);
+// const firebaseConfig = {
+//     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+//     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+//     projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+//     storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+//     messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+//     appId: process.env.REACT_APP_FIREBASE_APP_ID,
+// };
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAQQG7BUbki9nVp0rtrX5zKHsAXMtr8wE0",
+    authDomain: "ema-jhon-module-59.firebaseapp.com",
+    projectId: "ema-jhon-module-59",
+    storageBucket: "ema-jhon-module-59.appspot.com",
+    messagingSenderId: "573014030891",
+    appId: "1:573014030891:web:f1eba05608bf4a1c13886e"
+};
+
+export default firebaseConfig;
